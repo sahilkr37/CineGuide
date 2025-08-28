3c5134785c519620758b0658d32364b5f8b6f1f0
Sahil Kumar <sahilkumar012004@gmail.com>
2025-08-28 16:52:24 +0530

    Made MAinContainerDetails responsive

--- src/components/MainContainerDetails.jsx
+++ src/components/MainContainerDetails.jsx
@@ -11,25 +11,32 @@ function MainContainerDetails({ mainMovieId }) {
     if (!mainMovie) return null
 
     return (
-        <div className="absolute top-1/3 left-20 z-20 text-white max-w-xl pointer-events-none">
-            <h1 className="text-4xl sm:text-5xl font-extrabold drop-shadow-lg">
+        <div className="absolute top-1/3 sm:left-20 left-2 z-20 text-white max-w-xs sm:max-w-xl pointer-events-none">
+            {/* Title */}
+            <h1 className="text-lg sm:text-4xl md:text-5xl font-extrabold drop-shadow-lg leading-tight">
                 {mainMovie.title}
             </h1>
-            <p className="mt-4 text-sm sm:text-lg text-gray-200 line-clamp-3">
+
+            {/* Overview */}
+            <p className="mt-3 text-xs sm:text-base md:text-lg text-gray-200 line-clamp-3">
                 {mainMovie.overview}
             </p>
-            <div className="mt-6 flex gap-4 pointer-events-auto">
-                <button className="bg-white cursor-pointer text-black px-6 py-2 rounded-lg font-semibold hover:bg-gray-200">
+
+            {/* Buttons */}
+            <div className="mt-4 sm:mt-6 flex flex-wrap gap-2 sm:gap-4 pointer-events-auto">
+                <button className="bg-white cursor-pointer text-black px-2 sm:px-6 py-1 sm:py-2 rounded-md text-xs sm:text-base font-semibold hover:bg-gray-200">
                     ▶ Play Trailer
                 </button>
-                <button onClick={() => setLiked(!liked)} className="bg-gray-700 cursor-pointer bg-opacity-70 text-white px-6 py-2 rounded-lg font-semibold hover:bg-gray-600">
+                <button
+                    onClick={() => setLiked(!liked)}
+                    className="bg-gray-700 cursor-pointer bg-opacity-70 text-white px-3 py-1.5 sm:px-6 sm:py-2 rounded-lg text-sm sm:text-base font-semibold hover:bg-gray-600 flex items-center justify-center"
+                >
                     <Heart
-                        className={`  w-10 h-10 transition-all duration-100 ease-in-out transform ${liked
+                        className={`w-5 h-5 sm:w-10 sm:h-10 transition-all duration-200 ease-in-out transform ${liked
                             ? "fill-amber-600 text-amber-600 scale-125"
                             : "fill-transparent text-white scale-100"
                             }`}
                     />
-
                 </button>
             </div>
         </div>
